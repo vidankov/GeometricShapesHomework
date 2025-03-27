4566ee74480e0355ae2100e1ef2d171b737f7de4
vidankov <slavavinsent@gmail.com>
2025-03-27 12:47:05 +0300

    Реализован класс Triangle для представления треугольников. Реализовано вычисление площади.

---
+++ Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapesHomework
+{
+    internal class Triangle : TwoDimensionalShape, IGeometricShape<TwoDimensionalSide>
+    {
+        public double Square
+        {
+            get
+            {
+                double[] sides = Sides.Select(side => side.Length).ToArray();
+                double p = sides.Sum() / 2;
+                return Math.Sqrt(p * sides.Select(side => p - side).Aggregate((a, b) => a * b));
+            }
+        }
+    public Triangle(params List<TwoDimensionalSide> sides) : base(sides)
+    {
+    }
+}
+}
