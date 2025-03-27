46d6500d461b4249254ffc74379cb60c4669ca78
vidankov <slavavinsent@gmail.com>
2025-03-27 10:26:48 +0300

    Реализован базовый интерфейс IGeometricShape для геометрических фигур

---
+++ IGeometricShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapesHomework
+{
+    internal interface IGeometricShape
+    {
+        double Square { get; }
+        int Dimension { get; }
+        List<Side> Sides { get; }
+    }
+}
